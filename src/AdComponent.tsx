04b630fc8f39f175cda1a1791371b3f5f07fe1a9
Zafer AYAN <zaferayan@teknasyon.com>
2022-02-01 15:11:38 +0300

    Add

---
+++ src/AdComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {
+  AdIconView,
+  MediaView,
+  AdChoicesView,
+  TriggerableView,
+  withNativeAd,
+} from 'react-native-fbads';
+class AdComponent extends React.Component {
+  render() {
+    return (
+      <View>
+        <AdChoicesView style={{position: 'absolute', left: 0, top: 0}} />
+        <AdIconView style={{width: 300, height: 50}} />
+        <MediaView style={{width: 300, height: 200}} />
+        <TriggerableView>
+          <Text>{this.props.nativeAd.description}</Text>
+        </TriggerableView>
+      </View>
+    );
+  }
+}
+
+export default withNativeAd(AdComponent);
