e15a919880fb495250f9175649337f4c82fb03a0
Zafer AYAN <zaferayan@teknasyon.com>
2022-02-02 11:11:54 +0300

    Remove fbads

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import {View} from 'react-native';
-import {NativeAdsManager} from 'react-native-fbads';
+// import {NativeAdsManager} from 'react-native-fbads';
 import AdComponent from './src/AdComponent';
 
 class App extends React.Component {
-  adsManager = new NativeAdsManager('2122084034714087_2122088321380325');
+  // adsManager = new NativeAdsManager('2122084034714087_2122088321380325');
 
   render() {
     return (
       <View>
         {/* @ts-ignore */}
-        <AdComponent adsManager={this.adsManager} />
+        {/* <AdComponent adsManager={this.adsManager} /> */}
+        <AdComponent />
       </View>
     );
   }
--- src/AdComponent.tsx
+++ src/AdComponent.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {
-  AdIconView,
-  MediaView,
-  AdChoicesView,
-  TriggerableView,
-  withNativeAd,
-} from 'react-native-fbads';
+import {View} from 'react-native';
+// import {
+//   AdIconView,
+//   MediaView,
+//   AdChoicesView,
+//   TriggerableView,
+//   withNativeAd,
+// } from 'react-native-fbads';
 class AdComponent extends React.Component {
   render() {
     return (
       <View>
         <View style={{width: 100, height: 100, backgroundColor: 'red'}}></View>
-        <AdChoicesView style={{position: 'absolute', left: 0, top: 0}} />
+        {/* <AdChoicesView style={{position: 'absolute', left: 0, top: 0}} />
         <AdIconView style={{width: 300, height: 50}} />
         <MediaView style={{width: 300, height: 200}} />
         <TriggerableView>
           <Text>{this.props.nativeAd.description}</Text>
-        </TriggerableView>
+        </TriggerableView> */}
       </View>
     );
   }
 }
 
-export default withNativeAd(AdComponent);
+// export default withNativeAd(AdComponent);
+export default AdComponent;
