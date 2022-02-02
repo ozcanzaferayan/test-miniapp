9a101d0ecab03adab33d38c4d57b6b6c997a333f
Zafer AYAN <zaferayan@teknasyon.com>
2022-02-02 14:28:01 +0300

    Add crash button

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 // import {NativeAdsManager} from 'react-native-fbads';
 import AdComponent from './src/AdComponent';
 
 class App extends React.Component {
   // adsManager = new NativeAdsManager('2122084034714087_2122088321380325');
-
+  a = undefined;
   render() {
     return (
       <View>
         {/* @ts-ignore */}
         {/* <AdComponent adsManager={this.adsManager} /> */}
         <AdComponent />
+        <Button title="Crash" onPress={() => this.a.asdasd} />
       </View>
     );
   }
